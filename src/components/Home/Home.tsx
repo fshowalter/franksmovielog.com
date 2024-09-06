85886ccdb06e8536a480df9059071609e845c681
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2024-09-06 16:41:00 -0400

    wip

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,33 +9,30 @@ export interface Props {
 
 export function Home({ values }: Props): JSX.Element {
   return (
-    <main className="mx-auto max-w-[calc(1080px_+_2_*_var(--page-margin-width))]">
-      <div className="spacer-y-6 desktop:spacer-y-8" />
-      <PageTitle className="text-center">Latest Update</PageTitle>
-      <div className="spacer-y-6 desktop:spacer-y-8" />
-      <HomeSplash value={values[0]} />
-      <h3 className="mx-auto max-w-[960px] text-center text-[2rem]">
-        You Might Have Missed
-        <div className="spacer-y-6" />
-      </h3>
-      <ol className="flex flex-col">
-        {values.slice(1).map((value, index) => {
-          return (
-            <HomeListItem
-              key={value.sequence}
-              value={value}
-              eagerLoadImage={index === 0}
-            />
-          );
-        })}
-      </ol>
-      <div className="flex py-10">
-        <a
-          href="/reviews/"
-          className="mx-auto px-pageMargin py-2 tracking-wide text-accent shadow-all hover:shadow-border-accent"
-        >
-          All Reviews
-        </a>
+    <main className="bg-subtle">
+      <div className="mx-auto max-w-canvas">
+        <div className="spacer-y-6 desktop:spacer-y-8" />
+        <PageTitle className="text-center">Latest Updates</PageTitle>
+        <div className="spacer-y-6 desktop:spacer-y-8" />
+        <ul className="w-full tablet:grid tablet:w-auto tablet:grid-cols-[repeat(2,minmax(100px,312px))] tablet:gap-8 tablet:px-gutter desktop:max-w-unset desktop:grid-cols-[repeat(3,1fr)] desktop:px-pageMargin desktop:pt-2">
+          {values.map((value, index) => {
+            return (
+              <HomeListItem
+                key={value.sequence}
+                value={value}
+                eagerLoadImage={index === 0}
+              />
+            );
+          })}
+        </ul>
+        <div className="flex py-10">
+          <a
+            href="/reviews/"
+            className="mx-auto bg-default px-pageMargin py-2 tracking-wide text-accent shadow-all hover:shadow-border-accent"
+          >
+            All Reviews
+          </a>
+        </div>
       </div>
     </main>
   );
