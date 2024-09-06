6ea92aeba6444bade44fba678fc64bbfaa5a8dbe
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2024-09-06 15:56:04 -0400

    wip

--- src/components/Home/HomeListItem.tsx
+++ src/components/Home/HomeListItem.tsx
@@ -3,7 +3,6 @@ import type { StillImageProps } from "src/api/stills";
 import { Grade } from "src/components/Grade";
 import { RenderedMarkdown } from "src/components/RenderedMarkdown";
 import { Still } from "src/components/Still";
-import { toSentence } from "src/utils/toSentence";
 
 export const StillImageConfig = {
   width: 480,
@@ -33,43 +32,44 @@ export interface ListItemValue
     | "principalCastNames"
     | "directorNames"
     | "excerpt"
-  > {}
+    | "genres"
+  > {
+  stillImageProps: StillImageProps;
+}
 
 export function HomeListItem({
   value,
   eagerLoadImage,
-  stillImageProps: stillImageData,
 }: {
   value: ListItemValue;
   eagerLoadImage: boolean;
-  stillImageProps: StillImageProps | undefined;
 }) {
   return (
-    <li className="flex even:bg-subtle">
-      <article className="mx-auto flex flex-col items-center px-pageMargin py-10 desktop:grid desktop:w-full desktop:grid-cols-2 desktop:gap-x-[calc(2_*_var(--gutter-width))] max:grid-cols-7">
-        <div className="col-span-full mb-6 text-center text-sm font-light uppercase leading-4 tracking-0.75px text-subtle desktop:mb-0 desktop:pb-6 desktop:text-left desktop:leading-8 max:col-span-1 max:self-start">
+    <li className="flex max-w-canvas px-pageMargin odd:bg-subtle">
+      <article className="mx-auto flex max-w-[1080px] flex-col items-center py-10 desktop:grid desktop:w-full desktop:grid-cols-[1fr_minmax(min-content,_498px)] desktop:grid-rows-[auto_auto_1fr] desktop:gap-x-[var(--gutter-width)]">
+        <div className="pb-3 text-center text-sm font-light uppercase leading-4 tracking-0.75px text-subtle desktop:text-left desktop:leading-8">
           {formatDate(value.date)}
         </div>
         <a
           rel="canonical"
           href={`/reviews/${value.slug}/`}
-          className="still-border block max-w-prose desktop:col-start-2 desktop:col-end-2 desktop:row-span-2 desktop:row-start-2 desktop:self-start desktop:justify-self-end max:col-start-4 max:col-end-9 max:row-start-1 max:mt-0"
+          className="still-border block max-w-prose desktop:col-start-2 desktop:col-end-2 desktop:row-span-3 desktop:row-start-1 desktop:justify-end desktop:self-start"
         >
-          {stillImageData && (
+          {value.stillImageProps && (
             <Still
               title={value.title}
               year={value.year}
               width={StillImageConfig.width}
               height={StillImageConfig.height}
               sizes={StillImageConfig.sizes}
-              imageProps={stillImageData}
+              imageProps={value.stillImageProps}
               loading={eagerLoadImage ? "eager" : "lazy"}
-              className="h-auto rounded-xl"
+              className="h-auto"
               decoding={eagerLoadImage ? "sync" : "async"}
             />
           )}
         </a>
-        <div className="flex max-w-lg flex-col items-center pt-4 desktop:col-span-1 desktop:col-start-1 desktop:row-start-2 desktop:items-start desktop:place-self-start desktop:pt-0 max:col-span-3 max:col-start-2 max:row-start-1 max:max-w-unset">
+        <div className="flex max-w-prose flex-col items-center pt-4 desktop:col-span-1 desktop:col-start-1 desktop:row-start-2 desktop:items-start desktop:place-self-start desktop:pt-0">
           <h2 className="text-2.5xl font-bold leading-8">
             <a
               href={`/reviews/${value.slug}/`}
@@ -83,12 +83,17 @@ export function HomeListItem({
             </a>
           </h2>
           <div className="spacer-y-4" />
-          <Grade value={value.grade} height={32} />
+          <Grade value={value.grade} height={24} />
           <div className="spacer-y-6" />
-          <p className="text-base font-normal leading-normal tracking-0.25px text-subtle">
-            Directed by {toSentence(value.directorNames)}. Starring{" "}
-            {toSentence(value.principalCastNames)}.
-          </p>
+          <div className="text-sm leading-4 tracking-0.5px text-subtle">
+            {value.genres.map((genre, index) => {
+              if (index === 0) {
+                return <span key={genre}>{genre}</span>;
+              }
+
+              return <span key={genre}> | {genre}</span>;
+            })}
+          </div>
           <div className="spacer-y-6" />
           <RenderedMarkdown
             text={value.excerpt}
