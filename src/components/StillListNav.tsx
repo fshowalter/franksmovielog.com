83799a95122e8c5b251ec5f70d84fb5c5cf1f663
fshowalter <fshowalter@users.noreply.github.com>
2024-09-19 20:47:11 +0000

    refactor: apply lint autofix

--- src/components/StillListNav.tsx
+++ src/components/StillListNav.tsx
@@ -1,6 +1,6 @@
 export function StillListNav({ children }: { children: React.ReactNode }) {
   return (
-    <nav className="relative mx-auto flex w-full max-w-screen-max flex-col items-center px-[8%] desktop:px-20">
+    <nav className="relative mx-auto flex w-full max-w-screen-max flex-col items-center px-container-base desktop:px-20">
       {children}
     </nav>
   );
