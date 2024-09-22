91ffd03cb931ab09a2721e2bfa78868a45fca547
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2024-09-22 18:31:58 -0400

    better meta

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -152,7 +152,7 @@ function Meta({
   return (
     <div
       className={ccn(
-        "text-center font-sans-narrow text-xs font-medium uppercase tracking-[.6px] text-subtle",
+        "text-center font-sans-narrow text-xs font-medium uppercase tracking-[.6px] text-subtle desktop:text-sm desktop:font-normal",
         className,
       )}
     >
