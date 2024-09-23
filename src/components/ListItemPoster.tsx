969cac42eb61b084aaee9077d9b9c1f0535239a1
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2024-09-23 08:56:21 -0400

    better posters

--- src/components/ListItemPoster.tsx
+++ src/components/ListItemPoster.tsx
@@ -12,25 +12,6 @@ export function ListItemPoster({
   slug?: string | null;
   imageProps: PosterImageProps;
 }) {
-  if (slug) {
-    return (
-      <a
-        href={`/reviews/${slug}/`}
-        className="w-16 shrink-0 tablet:w-[76px] desktop:w-20"
-      >
-        <img
-          {...imageProps}
-          alt=""
-          width={ListItemPosterImageConfig.width}
-          height={ListItemPosterImageConfig.height}
-          loading="lazy"
-          decoding="async"
-          className="aspect-[0.66666667]"
-        />
-      </a>
-    );
-  }
-
   return (
     <div className="w-16 shrink-0 tablet:w-[76px] desktop:w-20">
       <img
