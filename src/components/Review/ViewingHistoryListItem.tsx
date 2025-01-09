95b80975a68d31624c6ace06af33d556ca8a881e
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-01-09 15:58:12 -0500

    update viewing history

--- src/components/Review/ViewingHistoryListItem.tsx
+++ src/components/Review/ViewingHistoryListItem.tsx
@@ -1,17 +1,8 @@
 import type { ReviewContent } from "~/api/reviews";
 
-import { DateIcon } from "~/components/DateIcon";
 import { RenderedMarkdown } from "~/components/RenderedMarkdown";
 
 type Viewing = ReviewContent["viewings"][0];
-
-const dateFormat = new Intl.DateTimeFormat("en-US", {
-  day: "numeric",
-  month: "short",
-  timeZone: "UTC",
-  weekday: "short",
-  year: "numeric",
-});
 
 const monthFormat = new Intl.DateTimeFormat("en-US", {
   month: "short",
@@ -35,15 +26,16 @@ const weekdayFormat = new Intl.DateTimeFormat("en-US", {
 
 export function ViewingHistoryListItem({ value }: { value: Viewing }) {
   return (
-    <li className="flex flex-col px-4 font-sans text-xs font-light even:bg-stripe">
-      <div className="flex items-center gap-x-[1ch] py-4">
+    <li className="flex flex-col font-sans text-xs font-light even:mx-[calc(-1_*_8vw)] even:bg-stripe even:px-[8vw] tablet:px-4 tablet:even:mx-0 tablet:even:px-4">
+      <div className="flex items-center gap-x-4 py-4 tablet:gap-x-6">
         <div className="size-auto">
           <Date date={value.date} />
         </div>
         <div className="grow">
-          <Medium value={value.medium} />{" "}
+          <Medium value={value.medium} />
           <MediumNotes value={value.mediumNotes} />
-          <Venue value={value.venue} /> <VenueNotes value={value.venueNotes} />
+          <Venue addArticle={!!value.medium} value={value.venue} />{" "}
+          <VenueNotes value={value.venueNotes} />
         </div>
       </div>
       <div>
@@ -56,24 +48,20 @@ export function ViewingHistoryListItem({ value }: { value: Viewing }) {
 function Date({ date }: { date: Date }) {
   return (
     <div className="bg-subtle p-2 text-center">
-      <div className="px-4 pb-2 font-normal text-subtle">
+      <div className="px-4 pb-2 font-serif text-md font-normal text-subtle">
         {yearFormat.format(date)}
       </div>
-      <div className="bg-default py-1">{monthFormat.format(date)}</div>
-      <div className="bg-default text-base font-normal text-subtle">
+      <div className="bg-default py-1 text-subtle">
+        {monthFormat.format(date)}
+      </div>
+      <div className="bg-default font-serif text-md font-normal text-default">
         {dayFormat.format(date)}
       </div>
-      <div className="bg-default py-1">{weekdayFormat.format(date)}</div>
+      <div className="bg-default py-1 text-subtle">
+        {weekdayFormat.format(date)}
+      </div>
     </div>
   );
-
-  // return (
-  //   <>
-  //     <span className="inline-block font-sans text-xs font-normal tracking-normal text-subtle">
-  //       {dateFormat.format(date)}
-  //     </span>{" "}
-  //   </>
-  // );
 }
 
 function Medium({ value }: { value: Viewing["medium"] }) {
@@ -81,9 +69,8 @@ function Medium({ value }: { value: Viewing["medium"] }) {
     return false;
   }
   return (
-    <span className="text-subtle">
-      {/* <span>via</span>{" "} */}
-      <span className="font-sans text-xs font-normal text-subtle">{value}</span>
+    <span className="font-serif text-base font-normal text-default">
+      {value}
     </span>
   );
 }
@@ -94,20 +81,29 @@ function MediumNotes({ value }: { value: Viewing["mediumNotes"] }) {
   }
   return (
     <span className="font-light tracking-normal text-subtle">
+      {" "}
       (
       <RenderedMarkdown as="span" className="leading-none" text={value} />)
     </span>
   );
 }
 
-function Venue({ value }: { value: Viewing["venue"] }) {
+function Venue({
+  addArticle,
+  value,
+}: {
+  addArticle: boolean;
+  value: Viewing["venue"];
+}) {
   if (!value) {
     return false;
   }
   return (
     <span className="text-subtle">
-      <span>at</span>{" "}
-      <span className="font-sans text-xs font-normal text-subtle">{value}</span>
+      {addArticle && <span> at </span>}
+      <span className="font-serif text-base font-normal text-default">
+        {value}
+      </span>
     </span>
   );
 }
@@ -129,7 +125,7 @@ function ViewingNotes({ value }: { value: Viewing["viewingNotes"] }) {
     return false;
   }
   return (
-    <div className="pb-6 text-sm font-light tablet:mx-8">
+    <div className="pb-6 text-sm font-light tablet:ml-28 tablet:mr-20">
       <RenderedMarkdown className="leading-normal text-muted" text={value} />
     </div>
   );
