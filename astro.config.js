5ca8a7b7bd79f7c4ccf40ba8d3c4f7c737fc7390
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-06-21 11:00:45 -0400

    wip

--- astro.config.js
+++ astro.config.js
@@ -1,5 +1,6 @@
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 import compressor from "astro-compressor";
 import { defineConfig } from "astro/config";
 import path from "node:path";
@@ -121,6 +122,7 @@ export default defineConfig({
       exclude: ["fsevents"],
     },
     plugins: [
+      tailwindcss(),
       contentHmr(),
       react({
         babel: {
