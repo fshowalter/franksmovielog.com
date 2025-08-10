d61e266b5fe8b9da393281c0fec74035d84663d6
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-08-10 09:50:05 -0400

    enforce max-workers

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,7 @@ export default getViteConfig({
       provider: "istanbul",
     },
     globals: true, // needed for testing-library teardown
+    maxWorkers: 2,
     projects: [
       {
         extends: true,
