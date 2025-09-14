a06ec3f1a96d73294a3a6de1fe37cd0322398947
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-09-14 13:51:57 -0400

    wip

--- src/features/reviews/selectHasActiveReviewsFilters.ts
+++
@@ -1,7 +0,0 @@
-import type { WatchlistFiltersValues } from "./Watchlist.reducer";
-
-export function selectHasActiveWatchlistFilters(
-  pendingFilterValues: WatchlistFiltersValues,
-): boolean {
-  return Object.keys(pendingFilterValues).length > 0;
-}
