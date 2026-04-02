5a642076eef43c1fd7fd1c78a10b7bb62b240ac8
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-04-01 21:56:29 -0400

    move `getCollection` calls into utilities and add module-level caching

--- src/utils/mostRecentReviewedTitles.ts
+++ src/utils/mostRecentReviewedTitles.ts
@@ -1,13 +1,23 @@
+import type { CollectionEntry } from "astro:content";
+
 import { getCollection } from "astro:content";
 
 import { collator } from "./collator";
 
+let cache: CollectionEntry<"reviewedTitles">[];
+
 export async function mostRecentReviewedTitles(limit: number) {
+  if (cache) {
+    return cache;
+  }
+
   const reviewedTitles = await getCollection("reviewedTitles");
 
   reviewedTitles.sort((a, b) => {
     return collator.compare(b.data.reviewSequence, a.data.reviewSequence);
   });
 
-  return reviewedTitles.slice(0, limit);
+  cache = reviewedTitles.slice(0, limit);
+
+  return cache;
 }
