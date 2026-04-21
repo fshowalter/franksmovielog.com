01f6b801f658f213d875e098668bb9b4d15a93ee
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-04-21 08:29:53 -0400

    code review

--- src/components/filter-and-sort/container/FilterAndSortToolbar.tsx
+++ src/components/filter-and-sort/container/FilterAndSortToolbar.tsx
@@ -114,14 +114,14 @@ export function FilterAndSortToolbar<T extends string>({
             className="size-6"
             fill="none"
             stroke="currentColor"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
               d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0-3.75-3.75M17.25 21 21 17.25"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </svg>
         </span>
