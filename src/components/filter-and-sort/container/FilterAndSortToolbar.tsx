71e290f8c84c9a9a9c569d4477058a322235e13b
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-04-21 08:41:19 -0400

    Add `aria-hidden` to filter icon and convert SVG attributes to JSX conventions

--- src/components/filter-and-sort/container/FilterAndSortToolbar.tsx
+++ src/components/filter-and-sort/container/FilterAndSortToolbar.tsx
@@ -111,12 +111,12 @@ export function FilterAndSortToolbar<T extends string>({
           "
         >
           <svg
+            aria-hidden="true"
             className="size-6"
             fill="none"
             stroke="currentColor"
-            strokeWidth="1.5"
+            strokeWidth={1.5}
             viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
           >
             <path
               d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0-3.75-3.75M17.25 21 21 17.25"
